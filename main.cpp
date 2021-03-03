92ba56a9e792418b2e02b33d6a80007c3c126952
Elomass <jean.elomass@gmail.com>
2021-03-03 20:02:33 +0100

    suite à lol test

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
+#include <iostream>
+#include <string>
+
 int main(int argc, char** argv)
 {
-    
+    std::cout<<"coucou elowan\n";
+    std::string loltest;
+    std::getline(std::cin, loltest);
+    std::cout<<loltest;
+    if (!loltest.empty())
+    {
+        std::cout<<"\n";
+    }
+    return 0;
 }
