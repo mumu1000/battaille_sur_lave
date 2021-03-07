b4d4cf51e2cb6cdd9ee4cdc0be38a4e664d3c49f
Elomass <jean.elomass@gmail.com>
2021-03-07 17:25:02 +0100

    continuation du test1

--- main.cpp
+++ main.cpp
@@ -4,33 +4,34 @@
 
 
 
-void ecrireConsole(std::string aEcrire)
-{
-    std::cout<<aEcrire;
-    if (!aEcrire.empty())
-    {
-        std::cout<<"\n";
-    }
-}
+// void ecrireConsole(std::string aEcrire)
+// {
+//     std::cout<<aEcrire;
+//     if (!aEcrire.empty())
+//     {
+//         std::cout<<"\n";
+//     }
+// }
 
 
 int main(int argc, char** argv)
 {
-    ecrireConsole("coucou elowan");
-    std::string loltest;
-    std::vector<std::string> tableauLignes;
-    do
-    {
-        std::getline(std::cin, loltest);
-        if(loltest != "STOP!")
-            tableauLignes.push_back(loltest) ;
+    // ecrireConsole("coucou elowan");
+    // std::string loltest;
+    // std::vector<std::string> tableauLignes;
+    // do
+    // {
+    //     std::getline(std::cin, loltest);
+    //     if(loltest != "STOP!")
+    //         tableauLignes.push_back(loltest) ;
         
-    } while (loltest != "STOP!");
+    // } while (loltest != "STOP!");
 
-    for(unsigned int i = 0; i<tableauLignes.size();i++)
-    {
-        ecrireConsole(tableauLignes[i]);
-    }
-    ecrireConsole("J'ai fini");
-    return 0;
+    // for(unsigned int i = 0; i<tableauLignes.size();i++)
+    // {
+    //     ecrireConsole(tableauLignes[i]);
+    // }
+    // ecrireConsole("J'ai fini");
+    // return 0;
+    
 }
