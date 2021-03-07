4c4645bdd178f7fda566a4739c6f2e345ad3741c
Amiel Rivier <amiel.rivier@gmail.com>
2021-03-07 17:43:27 +0100

    base exercice

--- main.cpp
+++ main.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <time.h>
 
+unsigned int nombreAleatoire(unsigned int min , unsigned int max)
+{
+    srand(time(0));
+    for (int i =0;i<100;i++)
+    {
+        rand();
+    }
+    return min + (rand() % (max+1-min));
+}
 
+unsigned int demanderNombre()
+{
+    std::string temp;
+    std::getline(std::cin, temp);
+    return atoi(temp.c_str());
+}
 
-// void ecrireConsole(std::string aEcrire)
-// {
-//     std::cout<<aEcrire;
-//     if (!aEcrire.empty())
-//     {
-//         std::cout<<"\n";
-//     }
-// }
+void ecrireConsole(std::string aEcrire)
+{
+    std::cout<<aEcrire;
+    if (!aEcrire.empty())
+    {
+        std::cout<<"\n";
+    }
+}
+
+void ecrireConsole(unsigned int aEcrire)
+{
+    std::cout<<aEcrire;
+    std::cout<<"\n";
+}
 
 
 int main(int argc, char** argv)
