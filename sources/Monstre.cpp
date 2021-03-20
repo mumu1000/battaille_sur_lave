95955b5fb1837562eb1fa01831efca8c7a5b14f5
Amiel Rivier <amiel.rivier@gmail.com>
2021-03-20 20:08:30 +0100

    hotfix

--- sources/Monstre.cpp
+++ sources/Monstre.cpp
@@ -1,4 +1,5 @@
 #include "Monstre.h"
+#include "utils.h"
 
 Monstre::Monstre()
 {
@@ -9,10 +10,16 @@ Monstre::Monstre()
 }
 void Monstre::attaquer(Combattant& cible)
 {
+    utils::ecrireConsole("Je suis un " + m_nom + " et je tape sur une cible avec une force de "+ std::to_string(m_degat) +" !");
     cible.defendre(m_degat);
 }
 void Monstre::defendre(int degat)
 {
     if(degat>m_armure)
+    {
         m_pv-=degat-m_armure;
+        utils::ecrireConsole("Je suis un " + m_nom + " et il ne me reste que " + std::to_string(m_pv) + " points de vie !");
+    }else{
+        utils::ecrireConsole("Je suis un " + m_nom + " et j'ai esquivé une attaque !");
+    }
 }
--- sources/Personnage.cpp
+++ sources/Personnage.cpp
@@ -1,5 +1,5 @@
 #include "Personnage.h"
-
+#include "utils.h"
 Personnage::Personnage()
 {
     m_pv=100;
@@ -15,12 +15,20 @@ Personnage::Personnage(std::string nom)
     m_armure=1;
     m_degat=5;
 }
+
 void Personnage::attaquer(Combattant& cible)
 {
+    utils::ecrireConsole("Je suis le personnage " + m_nom + " et je tape sur une cible avec une force de "+ std::to_string(m_degat) +" !");
     cible.defendre(m_degat);
 }
+
 void Personnage::defendre(int degat)
 {
     if(degat>m_armure)
+    {
         m_pv-=degat-m_armure;
+        utils::ecrireConsole("Je suis le personnage " + m_nom + " et il ne me reste que " + std::to_string(m_pv) + " points de vie !");
+    }else{
+        utils::ecrireConsole("Je suis le personnage " + m_nom + " et j'ai esquivé une attaque !");
+    }
 }
