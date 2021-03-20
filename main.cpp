60053ad3897b9f826a5877ea35156a0a882c2081
Elomass <jean.elomass@gmail.com>
2021-03-20 20:18:15 +0100

    jdr4

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 #include <string>
 #include <vector>
 #include <time.h>
-#include "headers/Personnage.h"
+#include "Personnage.h"
 #include "utils.h"
+#include "Monstre.h"
+#include "Personnage.h"
+#include "Squelette.h"
+#include "Zombie.h"
 
 unsigned int nombreAleatoire(unsigned int min , unsigned int max)
 {
@@ -19,4 +23,22 @@ int main(int argc, char** argv)
 {
     Personnage perso_principal(utils::demander_string());
     utils::ecrireConsole(perso_principal.m_nom);
+    Monstre monstre_test=Zombie();
+    while (monstre_test.m_pv>0 && perso_principal.m_pv>0)
+    {
+        perso_principal.attaquer(monstre_test);
+        if (monstre_test.m_pv>0)
+            monstre_test.attaquer(perso_principal);
+    }
+    if (perso_principal.m_pv<=0)
+    {
+        utils::ecrireConsole("vous etre mort. prout.");
+        return 0;
+    }
+    if (monstre_test.m_pv<=0)
+    {
+        utils::ecrireConsole("protchproutbleurk");
+        return 0;
+    }
+    return 0;
 }
