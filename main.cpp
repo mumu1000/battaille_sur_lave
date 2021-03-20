7dcbbca02e1d193f944d039b071b1b5e0215bfe8
Amiel Rivier <amiel.rivier@gmail.com>
2021-03-20 19:41:48 +0100

    clean main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <time.h>
 #include "headers/Personnage.h"
+#include "utils.h"
+
 unsigned int nombreAleatoire(unsigned int min , unsigned int max)
 {
     srand(time(0));
@@ -13,36 +15,8 @@ unsigned int nombreAleatoire(unsigned int min , unsigned int max)
     return min + (rand() % (max+1-min));
 }
 
-unsigned int demanderNombre()
-{
-    std::string temp;
-    std::getline(std::cin, temp);
-    return atoi(temp.c_str());
-}
-std::string demander_string()
-{
-    std::string temp;
-    std::getline(std::cin, temp);
-    return temp;
-}
-void ecrireConsole(std::string aEcrire)
-{
-    std::cout<<aEcrire;
-    if (!aEcrire.empty())
-    {
-        std::cout<<"\n";
-    }
-}
-
-void ecrireConsole(unsigned int aEcrire)
-{
-    std::cout<<aEcrire;
-    std::cout<<"\n";
-}
-
-
 int main(int argc, char** argv)
 {
-    Personnage perso_principal(demander_string());
-    ecrireConsole(perso_principal.m_nom);
+    Personnage perso_principal(utils::demander_string());
+    utils::ecrireConsole(perso_principal.m_nom);
 }
